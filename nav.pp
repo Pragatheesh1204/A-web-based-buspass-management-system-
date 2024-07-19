efb5535a32f29759ea91269c0e5bb54c4b3dffa9
Pragatheesh1204 <153920032+Pragatheesh1204@users.noreply.github.com>
2024-07-19 21:23:17 +0530

    nav.php

---
+++ nav.pp
@@ -0,0 +1,20 @@
+<form id="form1" runat="server">
+  <!-- <img src="b1.jpg" alt="bus"  /> -->
+  <ul>
+    <P style="color:white">
+    BUS PASS MANAGEMENT SYSTEM
+
+    </P>
+    <!-- <li><a href="info.php">Pass Information</a></li> -->
+    <!-- <li><a href="ustu.php">Student Renewal</a></li>
+  <li><a href="vdriver.php">View Driver Details</a></li>
+  <li><a href="vbus.php">View Bus Details</a></li>
+  <li><a href="vstu.php">View Student Details</a></li>
+  <li><a href="driver.php">Driver Register</a></li>
+  <li><a href="bus.php">Bus Register</a></li>
+  <li><a href="stu.php">Student Register</a></li> -->
+  <li><a href="./student.php">Student</a></li>
+    <li><a href="./admin.php">Admin</a></li>
+    <li><a href="./index.php">Home</a></li>
+  </ul>
+</form>
